e45cd159bc7a436bc849d0d337349510da5f4111
RosenblumLabUser <79983570+RosenblumLabUser@users.noreply.github.com>
2024-12-12 09:59:01 +0200

    updated Hfss configuration

--- src/pysubmit/simulation/analyze.py
+++ src/pysubmit/simulation/analyze.py
@@ -12,27 +12,28 @@ def main(config: Config, flags=None):
     # check status
     # TODO: add context manager for hfss so it will be saved using final
     config_project = config.config_project
-    with Hfss(version=config_project.version,
-              new_desktop=False,
-              design=config_project.design_name,
-              project=config_project.path,
-              non_graphical=True) as hfss:
+    with Hfss(version=config_project.version, new_desktop=False,
+              design=config_project.design_name, project=config_project.path,
+              close_on_exit=True, remove_lock=True, non_graphical=True) as hfss:
+        
+        _analysis(hfss, config)
 
-        # change variables accordingly
-        variable_handler.set_variables(hfss, config.hfss_variables)
 
-        # call for classical simulation
-        mode_to_freq_and_q_factor = classical_run(hfss)
-        json_write('classical_results.json', mode_to_freq_and_q_factor)
+def _analysis(hfss: Hfss, config: Config):
+    # change variables accordingly
+    variable_handler.set_variables(hfss, config.hfss_variables)
 
-        # save results
-        for modes_and_labels in config.modes_and_labels:
-            mode_to_labels = modes_and_labels.parse(mode_to_freq_and_q_factor)
+    # call for classical simulation
+    mode_to_freq_and_q_factor = classical_run(hfss)
+    json_write('classical_results.json', mode_to_freq_and_q_factor)
 
-            # call for quantum simulation
-            quantum_result = quantum_run(hfss, mode_to_labels, config.junctions)
+    # save results
+    for modes_and_labels in config.modes_and_labels:
+        mode_to_labels = modes_and_labels.parse(mode_to_freq_and_q_factor)
 
-            # infer name
-            result_name = '_'.join(mode_to_labels.values())
-            json_write(f'{result_name}.json', quantum_result)
+        # call for quantum simulation
+        quantum_result = quantum_run(hfss, mode_to_labels, config.junctions)
 
+        # infer name
+        result_name = '_'.join(mode_to_labels.values())
+        json_write(f'{result_name}.json', quantum_result)
