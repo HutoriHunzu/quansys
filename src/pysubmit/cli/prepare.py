c417a007f82b0b7e98a84914fe37ae4b5f590db5
RosenblumLabUser <79983570+RosenblumLabUser@users.noreply.github.com>
2025-01-26 17:24:05 +0200

    removed date folder

--- src/pysubmit/cli/prepare.py
+++ src/pysubmit/cli/prepare.py
@@ -1,17 +1,13 @@
 from pathlib import Path
-from .utils import generate_timestamp, copy_files
+from .utils import copy_files
 from .templates import generate_job_submission_script, generate_simulation_script
 
 def prepare_job(config, files, mem, timeout):
     """
     Prepare the workflow: create directories, copy files, generate scripts.
     """
-    from .templates import generate_job_submission_script, generate_simulation_script
-    from .utils import generate_timestamp, copy_files
-
-    # Create project directory with timestamp
-    timestamp = generate_timestamp()
-    project_dir = Path(config.data_parameters.project_name) / timestamp
+    # Create the project directory
+    project_dir = Path(config.data_parameters.project_name)
     project_dir.mkdir(parents=True, exist_ok=True)
 
     # Save updated config.yaml to the project directory
@@ -31,7 +27,7 @@ def prepare_job(config, files, mem, timeout):
     copy_files(all_files_to_copy, project_dir)
 
     # Generate cluster scripts
-    generate_job_submission_script(project_dir, config, mem * 1024, timeout)  # Convert GB to MB
+    generate_job_submission_script(project_dir, config, mem, timeout)
     generate_simulation_script(project_dir)
 
     return project_dir.resolve()
