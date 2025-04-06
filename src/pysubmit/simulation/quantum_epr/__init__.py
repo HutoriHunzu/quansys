0c64fdb3d9ae918a57fed94e08f7748ab763f6f2
RosenblumLabUser <79983570+RosenblumLabUser@users.noreply.github.com>
2025-04-06 08:38:06 +0300

    some cleaning at quantum eprinit

--- src/pysubmit/simulation/quantum_epr/__init__.py
+++ src/pysubmit/simulation/quantum_epr/__init__.py
@@ -1,3 +1,3 @@
 from .model import QuantumEpr, QuantumResult
 from .structures import ConfigJunction
-from .modes_and_labels import ModesAndLabels, Modes, ManualInference, OrderInference
+from .modes_and_labels import ModesAndLabels, ManualInference, OrderInference
