0505475dbb3505d9ecec76605c59aa4e5ffb6387
HutoriHunzu <uri.goldblatt@gmail.com>
2025-08-07 12:49:57 +0300

    exclude idea

--- dep/run.py
+++
@@ -1,9 +0,0 @@
-def run_job(config_path):
-    # Use cached imports for better performance on repeated calls
-    from .cache import cached_import
-    
-    WorkflowConfig = cached_import('quansys.workflow', attr_name='WorkflowConfig')
-    execute_workflow = cached_import('quansys.workflow', attr_name='execute_workflow')
-
-    config = WorkflowConfig.load_from_yaml(config_path)
-    execute_workflow(config)
